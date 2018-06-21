96f5dc171f03be995167dcbffc7f382ce7e7f3be
Mickaël Misbach <mickael@thehyve.nl>
2018-06-21 14:47:12 +0200

    PIC-SURE services modifications in main component and module

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -38,6 +38,8 @@ import {GrowlModule} from 'primeng/growl';
 import {GbMainModule} from './modules/gb-main-module/gb-main.module';
 import {MessageHelper} from './utilities/message-helper';
 import {Observable} from 'rxjs/Observable';
+import {PicSureResourceService} from './services/picsure-services/picsure-resource.service';
+import {PicSureResourceServiceMock} from './services/mocks/picsure-resource.service.mock';
 
 export function initConfig(config: AppConfig) {
   return () => config.load();
@@ -122,6 +124,10 @@ describe('AppComponent', () => {
         {
           provide: ExportService,
           useClass: ExportServiceMock
+        },
+        {
+          provide: PicSureResourceService,
+          useClass: PicSureResourceServiceMock
         }
       ]
     }).compileComponents();
